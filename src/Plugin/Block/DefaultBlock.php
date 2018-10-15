e0f07bb3e45881cb1db9f0aac26287e1472ed12c
Theo <theo.lundy@niji.fr>
2018-10-15 18:14:11 +0200

    Init 2

---
+++ src/Plugin/Block/DefaultBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\cci_test2\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'DefaultBlock' block.
+ *
+ * @Block(
+ *  id = "default_block",
+ *  admin_label = @Translation("Default block"),
+ * )
+ */
+class DefaultBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#theme' => 'cci_agenda',
+      '#content_test' => "C'est un test"
+    );
+
+  }
+
+}
